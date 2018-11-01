2e1478a61fef9bbf354328dde7b35a32aeed0a1b
ali_cramstack <mazg1493@gmail.com>
2018-11-01 13:45:27 +0600

    Used get session display for sessions

--- django_file_upload/capacity/serializers.py
+++ django_file_upload/capacity/serializers.py
@@ -5,12 +5,16 @@ from django_file_upload.capacity.models import MachineDay, SAH, Pcs, GGPcs
 
 class MachineDaySerializer(serializers.ModelSerializer):
 
+    session = serializers.CharField(source='get_session_display')
+
     class Meta:
         model = MachineDay
         exclude = []
 
 
 class SAHSerializer(serializers.ModelSerializer):
+
+    session = serializers.CharField(source='get_session_display')
 
     class Meta:
         model = SAH
@@ -19,12 +23,16 @@ class SAHSerializer(serializers.ModelSerializer):
 
 class PcsSerializer(serializers.ModelSerializer):
 
+    session = serializers.CharField(source='get_session_display')
+
     class Meta:
         model = Pcs
         exclude = []
 
 
 class GGPcsSerializers(serializers.ModelSerializer):
+
+    session = serializers.CharField(source='get_session_display')
 
     class Meta:
         model = GGPcs
--- django_file_upload/confirmation/serializers.py
+++ django_file_upload/confirmation/serializers.py
@@ -5,6 +5,8 @@ from django_file_upload.confirmation.models import BuyerWise
 
 class BuyerWiseSerializer(serializers.ModelSerializer):
 
+    session = serializers.CharField(source='get_session_display')
+
     class Meta:
         model = BuyerWise
         exclude = []

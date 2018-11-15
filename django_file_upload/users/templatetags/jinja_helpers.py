6772ee3ee307280af20436a6aea377102fe0719a
ali_cramstack <mazg1493@gmail.com>
2018-11-15 17:16:12 +0600

    added debug print for sah values

--- django_file_upload/users/templatetags/jinja_helpers.py
+++ django_file_upload/users/templatetags/jinja_helpers.py
@@ -1,5 +1,6 @@
 from django_jinja import library
 
+from django_file_upload.capacity.models import SAH
 from django_file_upload.core.config import EXCLUDE_FIELDS, Session
 
 
@@ -10,7 +11,12 @@ def get_model_name(value):
 
 @library.global_function
 def get_field_value(data, field):
+
     value = getattr(data, field)
+    if type(data) == SAH and field == "budget":
+        print("Data: ", data)
+        print("Field: ", field)
+        print("Value: ", value)
     return round(value, 2) if value is not None else ''
 
 
